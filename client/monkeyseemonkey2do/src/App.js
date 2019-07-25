95a7bd80674d19731e4098eef832441e55affdfe
hongliao xiong <hongliaoxiong@hongliaos-MacBook-Air.local>
2019-07-24 21:10:18 -0700

    installed dependencies

--- client/monkeyseemonkey2do/src/App.js
+++ client/monkeyseemonkey2do/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./pages/Saved";
+import HomePage from "./pages/HomePage";
+import NoMatch from "./pages/NoMatch";
+import NavTabs from "./components/NavTabs/NavTabs";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
+import Footer from "./components/Footer/Footer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+
+        <div>
+          <NavTabs />
+          <Jumbotron />
+
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+           
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/saved/:id" component={Saved} />
+       
+            <Route component={NoMatch} />
+         
+          </Switch>
+          
+          <Footer />
+
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
