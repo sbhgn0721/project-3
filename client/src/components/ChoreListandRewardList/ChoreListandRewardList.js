9e07e1e6b2a2185ca9176a170b6f9c70a1cdfb28
nathan meyer <nmeyer@D-10-19-171-133.dhcp4.washington.edu>
2019-08-03 12:12:48 -0700

    halfway on style

--- client/src/components/ChoreListandRewardList/ChoreListandRewardList.js
+++ client/src/components/ChoreListandRewardList/ChoreListandRewardList.js
@@ -4,8 +4,10 @@ import "./ChoreListandRewardList.css";
 const ChoreListandRewardList =() =>(
     
         <div>
-            <a href="/parent/chores/" className="btn btn-primary" id="choreList-button"></a>
-            <a href="/parent/rewards/" className="btn btn-success" id="rewardList-button"></a>
+            {/* // eslint-disable-next-line */}
+            <a href="/parent/chores/" className="btn btn-secondary" id="choreList-button"><span>Chore List</span></a>
+            {/* // eslint-disable-next-line */}
+            <a href="/parent/rewards/" className="btn btn-secondary" id="rewardList-button"><span>Reward List</span></a>
 
         </div>
     )
