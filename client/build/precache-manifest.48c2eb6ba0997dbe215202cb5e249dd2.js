3296f706747e05d834c8a362d8bfe4b1341c5d9d
hongliao xiong <hongliaoxiong@hongliaos-MacBook-Air.local>
2019-08-02 23:53:25 -0700

    removed build folder from gitignore for deployment

---
+++ client/build/precache-manifest.48c2eb6ba0997dbe215202cb5e249dd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33344c58594a4df59f3c240708129e7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2c81bd05a9ca89d011a",
+    "url": "/static/css/main.e92ea222.chunk.css"
+  },
+  {
+    "revision": "a6a487172c6ffdc5b2b9",
+    "url": "/static/js/2.052304d9.chunk.js"
+  },
+  {
+    "revision": "a2c81bd05a9ca89d011a",
+    "url": "/static/js/main.1e50cf89.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
