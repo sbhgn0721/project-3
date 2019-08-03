4440992ed034fde0f902b86a8e3f182446e4020f
nathan meyer <nmeyer@D-10-19-171-133.dhcp4.washington.edu>
2019-08-03 11:35:06 -0700

    styles

--- client/src/components/zz_Icebox/EditBtn/EditBtn.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from "react";
-import "./style.css";
-
-
-
-export default EditBtn;
--- client/src/components/zz_Icebox/Header/Header.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ".style.css";
-
-
-const Header = props =>
-<div className="header">{props.children}</div>;
-
-export default Header;
