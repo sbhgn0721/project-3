f8dbbeac9ad4bdac2ee464770e40b67f314ea04c
hongliao xiong <hongliaoxiong@hongliaos-MacBook-Air.local>
2019-08-05 12:21:04 -0700

    try d

--- client/src/utils/Auth.js
+++ client/src/utils/Auth.js
@@ -7,7 +7,7 @@ class Auth {
       domain: 'young-wave-7408.auth0.com',
       audience: 'https://young-wave-7408.auth0.com/api/v2/',
       clientID: 'mO4o82RWTjPlqoNtm_Ub9zIBk7VRKoin',
-      redirectUri: 'http://still-tor-34851.herokuapp.com/callback',
+      redirectUri: `${window.location.origin}/callback`,
       responseType: 'id_token',
       scope: 'openid profile'
     });
