90dd94ed8f782e264571c0a072843c1242fa33ef
hongliao xiong <hongliaoxiong@hongliaos-MacBook-Air.local>
2019-08-05 23:03:27 -0700

    add server

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ if (process.env.NODE_ENV === "production") {
 
 }
 
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 
 app.use(routes);
 
