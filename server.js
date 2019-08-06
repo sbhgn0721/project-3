d756c5dcccabef87298a8e796cc57833f93cb62f
hongliao xiong <hongliaoxiong@hongliaos-MacBook-Air.local>
2019-08-05 22:46:44 -0700

    try

--- server.js
+++ server.js
@@ -7,13 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("*", (req, res) => {
-    let url = path.join(__dirname, '../client/build', 'index.html');
-    if (!url.startsWith('/app/')) // since we're on local windows
-      url = url.substring(1);
-    res.sendFile(url);
-  });
-  
+
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 
