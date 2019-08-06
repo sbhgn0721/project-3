0566c8003bee3289a820ef884e1f30e1fd318550
hongliao xiong <hongliaoxiong@hongliaos-MacBook-Air.local>
2019-08-05 20:56:09 -0700

    ty

--- client/src/utils/Auth.js
+++ client/src/utils/Auth.js
@@ -9,6 +9,7 @@ class Auth {
       domain: 'young-wave-7408.auth0.com',
       audience: 'https://young-wave-7408.auth0.com/api/v2/',
       clientID: 'mO4o82RWTjPlqoNtm_Ub9zIBk7VRKoin',
+      responseType: 'code',
       redirectUri: 'https://still-tor-34851.herokuapp.com/callback',
       scope: 'openid profile'
     });
