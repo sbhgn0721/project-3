b09daf5f93239e862201d7ba3754ab0c5ffb5370
Greg Lanter <dohnut22.gl@gmail.com>
2019-08-06 19:05:35 -0700

    replace missing lines in server.js lines 10-13

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("./client/public"));
+
+}
 app.use(express.static("./client/build"));
 
 app.use(routes);
