a09f3218def53c44019a7f08d353e494219f8a97
SITRUK <kurtis.santillanes@gmail.com>
2019-08-07 06:56:35 -0700

    committing changes to remote branch

--- client/src/components/ChoreListandRewardList/ChoreListandRewardList.js
+++ client/src/components/ChoreListandRewardList/ChoreListandRewardList.js
@@ -4,14 +4,9 @@ import "./ChoreListandRewardList.css";
 const ChoreListandRewardList =() =>(
     
         <div>
-<<<<<<< HEAD
-            <a href="/parent/chores/" className="btn btn-secondary" id="choreList-button"><span>Chore List</span></a>
-            <a href="/parent/rewards/" className="btn btn-secondary" id="rewardList-button"><span>Reward List</span></a>
-=======
             <a href="/parent/chores/" className="btn btn-secondary parentButton" id="choreList-button"><span>Chore List</span></a>
             <a href="/parent/rewards/" className="btn btn-secondary parentButton" id="rewardList-button"><span>Reward List</span></a>
 
->>>>>>> origin
         </div>
     )
 
