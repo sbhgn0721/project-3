3b2abe877c326fc047d9e9ba9a4e877b36be960b
Kurtis Santillanes <Kurtis.Santillanes@indigoslate.com>
2019-08-07 18:51:12 -0700

    changing the build scripts to mirror local

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.export = {
+    const dbURI = process.env.MONGODB_URI || "mongodb+srv://bananaAdmin:bananaSplit@users-shard-00-01-uhpur.mongodb.net:27017",
+    secretOrKey: "secret"
+}
--- server.js
+++ server.js
@@ -1,40 +1,82 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const routes = require("./routes");
-const app = express()
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const app = express()
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/public"));
+// Bodyparser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-}
-//line removed for pull request for child page
-//app.use(express.static("./client/build"));
 
+// DB Config
+const db = require("./config/keys").dbURI;
+
+// Connect to MongoDB
+mongoose
+.connect(
+    db,
+    { useNewUrlParser: true })
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+    
+    // Serve up static assets (usually on heroku)
+    if (process.env.NODE_ENV === "production") {
+        app.use(express.static("client/build"));
+    }
+    
+// Routes
+// app.use("/api/users", users);
 app.use(routes);
 
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-mongoose.Promise = Promise;
+
+app.listen(port, () => console.log(`Server up and running on port ${PORT} !`));
+
+
+// //// replacing in favor of bodyParser - false
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+
+// //// replacing in favor of my prod/build scripts
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/public"));
+
+// }
+
+// app.use(express.static("./client/build"));
+
+
+
+// mongoose.Promise = Promise;
 
 //const dbURI = process.env.MONGODB_URI || "mongodb://monkeyseemonkey2do:monkeyseemonkey2do@ds033037.mlab.com:33037/heroku_2rbklfzx";
-const dbURI = process.env.MONGODB_URI || "mongodb://localhost/monkeySeeMonkey2Dodb";
-mongoose.set('useCreateIndex', true)
+// const dbURI = process.env.MONGODB_URI || "mongodb+srv://bananaAdmin:bananaSplit@users-uhpur.mongodb.net/test?retryWrites=true&w=majority";
+// const dbURI = process.env.MONGODB_URI || "mongodb+srv://bananaAdmin:bananaSplit@users-shard-00-01-uhpur.mongodb.net:27017";
 
-mongoose.connect(dbURI, { useNewUrlParser: true })
 
-const db = mongoose.connection;
+// mongoose.set('useCreateIndex', true)
 
-db.on("error", function (error) {
-    console.log("Mongoose Error: ", error);
-})
+// mongoose.connect(dbURI, { useNewUrlParser: true })
 
-db.once("open", function () {
-    console.log("Mongoose connection successful.")
+// const db = mongoose.connection;
 
-    app.listen(PORT, function () {
-        console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-    });
-})
+// db.on("error", function (error) {
+//     console.log("Mongoose Error: ", error);
+// })
+
+// db.once("open", function () {
+//     console.log("Mongoose connection successful.")
+
+//     app.listen(PORT, function () {
+//         console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+//     });
+// })
