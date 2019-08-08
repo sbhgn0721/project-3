cd628a59411ff63624e3ddc7bcd58441737d194e
hongliao xiong <hongliaoxiong@hongliaos-MacBook-Air.local>
2019-08-08 02:20:24 -0700

    fix edit modal style

--- client/src/components/ChoreModal/ChoreModal.js
+++ client/src/components/ChoreModal/ChoreModal.js
@@ -7,12 +7,14 @@ const ChoreModal = props => {
             <div className="modal fade" id="editChore" tabIndex="-1" role="dialog">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
+                        {/*
                         <div className="modal-header">
                             <h5 className="modal-title" id="editChoreLabel">Edit Chore</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
+                        */}
                         <div className="modal-body">
                             <form id="editChoreForm">
                                 <div className="form-group">
